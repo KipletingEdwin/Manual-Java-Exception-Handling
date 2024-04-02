4b985b6d421889a7fbae46f9ebd3af384cb59a09
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-02 22:22:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+        }
+        catch (ArithmeticException e) {
+            System.out.println("You can't divide by zero!");
+        }
+    }
+}
